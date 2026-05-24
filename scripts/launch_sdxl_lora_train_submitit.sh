57be8a83580255617bb4bb989d4eba52b38697a2
AtarSander <szymczyk.alek1@gmail.com>
2026-05-24 18:55:42 +0200

    Updated the bash submission script

--- scripts/launch_sdxl_lora_train_submitit.sh
+++ scripts/launch_sdxl_lora_train_submitit.sh
@@ -5,7 +5,8 @@ if [[ -x /net/people/plgrid/plgatarsander/.local/bin/uv ]]; then
     --config-name train_submitit \
     --multirun \
     'hydra.sweep.dir=slurm_runs/${now:%Y-%m-%d}/${now:%H-%M-%S}' \
-    hydra.job.name=sdxl_lora_train
+    hydra.job.name=sdxl_lora_train \
+    "$@"
 fi
 
 echo "'uv' was not found at /net/people/plgrid/plgatarsander/.local/bin/uv." >&2
